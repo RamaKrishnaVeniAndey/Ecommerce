ff20495880e6c53158bc1130ca8563917799beb1
RamaKrishnaVeniAndey <137883949+RamaKrishnaVeniAndey@users.noreply.github.com>
2023-08-07 06:49:50 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Meesho from './Component/Meesho'
+
+function App() {
+  return (
+    <div className="App">
+     <Meesho />
+     
+    </div>
+  );
+}
+
+export default App;
